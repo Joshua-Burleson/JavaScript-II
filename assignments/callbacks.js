c0869c06c3d48241c77ee822dfa549567159246b
Joshua-Burleson <joshua.burleson.developer@gmail.com>
2019-09-01 13:23:27 -0400

    MVP and all stretch goals completed

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -41,23 +41,28 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.getLength);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr.slice(-1));
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x+y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.find(listItem => listItem === item) != undefined);
 }
 
 /* STRETCH PROBLEM */
@@ -66,4 +71,9 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  cb(array.filter((item, index) => {
+    //Use indexOf to return only the first instance of the searched item
+    //alternatively, lastIndexOf could be used but would alter the original order of the array
+    return index === array.indexOf(item);
+  }));
 }
--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,24 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+const enemiesOfTheState = function(){
+  let enemies = [];
+
+  return (newEnemy) => {
+    enemies.push(newEnemy);
+    let modifiedEnemies = enemies.map((enemy, index) => {
+      return enemies.length > 1 && index === enemies.length -1 ? ` and ${enemy}` : ` ${enemy}`;
+    });
+
+    return `These are the enemies of the state: ${[...modifiedEnemies]}`;
+  }
+}
+
+let aNewEnemy = enemiesOfTheState();
+
+console.log(aNewEnemy('Joshua'));
+console.log(aNewEnemy('Humberto'));
+console.log(aNewEnemy('Michael'));
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -16,18 +34,76 @@ const counterMaker = () => {
   //      NOTE: This `counter` function, being nested inside `counterMaker`,
   //      "closes over" the `count` variable. It can "see" it in the parent scope!
   // 3- Return the `counter` function.
+  let count = 0;
+
+  let counter = function(){
+    count++
+    return count;
+  }
+
+  return counter;
 };
-// Example usage: const myCounter = counterMaker();
-// myCounter(); // 1
-// myCounter(); // 2
+
+
+// Example usage: 
+const myCounter = counterMaker();
+console.log(myCounter()); // 1
+console.log(myCounter()); // 2
 
 // ==== Challenge 3: Make `counterMaker` more sophisticated ====
 // It should have a `limit` parameter. Any counters we make with `counterMaker`
 // will refuse to go over the limit, and start back at 1.
+
+const sophCounterMaker = () => {
+  let count = 0;
+
+  let counter = function(){
+    if(count === 5){
+      count = 0;
+    }
+    count++
+    return count;
+  }
+
+  return counter;
+};
+
+const mySecondCounter = sophCounterMaker();
+console.log(mySecondCounter()); // 1
+console.log(mySecondCounter()); // 2
+console.log(mySecondCounter()); // 3
+console.log(mySecondCounter()); // 4
+console.log(mySecondCounter()); // 5
+console.log(mySecondCounter()); // 1
 
 // ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let value = 0;
+
+  const calculator ={
+    increment: () => {
+      value ++;
+      return value;
+    },
+    decrement: () => {
+      value --;
+      return value;
+    }
+  }
+
+  return calculator;
 };
+
+let factory = counterFactory();
+
+console.log(factory.increment())
+console.log(factory.increment())
+console.log(factory.increment())
+
+console.log(factory.decrement())
+console.log(factory.decrement())
+console.log(factory.decrement())
+
